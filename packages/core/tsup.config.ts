3b1b2ea24a0404642f49bd7f4f371bba6b3ed742
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-07-09 02:11:32 -0300

    update some repo refs

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-	entry: ["src/**/*.ts", "!src/**/*.test.ts", "!src/css.d.ts"],
+	entry: ["src/**/*.ts", "!src/css.d.ts"],
 	format: ["esm"],
 	target: "esnext",
 	dts: true,

a8bcb1c0717804e28e0dafb0fbcc56ccbd8ac7a7
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2026-04-12 18:22:48 -0300

    update example app

--- apps/web/src/routes/auth/$.ts
+++ apps/web/src/routes/auth/$.ts
@@ -1,12 +1,6 @@
 import { issuer } from "@draftlab/auth/core"
-import { CodeProvider } from "@draftlab/auth/provider/code"
-import { MagicLinkProvider } from "@draftlab/auth/provider/magiclink"
-import { PasswordProvider } from "@draftlab/auth/provider/password"
+import { GithubProvider } from "@draftlab/auth/provider/github"
 import { MemoryStorage } from "@draftlab/auth/storage/memory"
-import { CodeUI } from "@draftlab/auth/ui/code"
-import { MagicLinkUI } from "@draftlab/auth/ui/magiclink"
-import { PasswordUI } from "@draftlab/auth/ui/password"
-import { Select } from "@draftlab/auth/ui/select"
 import { createFileRoute } from "@tanstack/react-router"
 import { subjects } from "@/libs/auth"
 
@@ -19,56 +13,20 @@ export const auth = issuer({
 		refresh: 604800,
 		retention: 1209600
 	},
-	select: Select({
-		copy: { button_provider: " " },
-		displays: {
-			code: "One-Time Code",
-			magiclink: "Magic Link",
-			password: "Email/Password"
-		}
-	}),
 	storage: MemoryStorage({
 		persist: "./persist.json"
 	}),
 	providers: {
-		magiclink: MagicLinkProvider(
-			MagicLinkUI({
-				sendLink: async (claims, link) => {
-					console.log(claims, link)
-				}
-			})
-		),
-		code: CodeProvider(
-			CodeUI({
-				sendCode: async (claims, code) => {
-					console.log(claims, code)
-				}
-			})
-		),
-		password: PasswordProvider(
-			PasswordUI({
-				async sendCode(email, code, context) {
-					console.log(`[${context}] Code for ${email}: ${code}`)
-				}
-			})
-		)
+		github: GithubProvider({
+			scopes: ["user:email", "read:user"],
+			clientID: process.env.GITHUB_CLIENT_ID ?? "",
+			clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ""
+		})
 	},
 	success: (ctx, value) => {
-		if (value.provider === "password") {
+		if (value.provider === "github") {
 			return ctx.subject("user", {
-				email: value.email
-			})
-		}
-
-		if (value.provider === "code") {
-			return ctx.subject("user", {
-				email: value.claims.email as string
-			})
-		}
-
-		if (value.provider === "magiclink") {
-			return ctx.subject("user", {
-				email: value.claims.email
+				email: value.tokenset.access
 			})
 		}
 
